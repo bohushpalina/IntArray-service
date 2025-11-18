2a10fdee53b5f4f9c8c514e0ba58aae090715990
bohushpalina <palinabohush@gmail.com>
2025-11-18 22:16:08 +0300

    Changed after checking

--- src/main/java/com/bohush/ft/reader/IntArrayReader.java
+++ src/main/java/com/bohush/ft/reader/IntArrayReader.java
@@ -1,7 +1,9 @@
 package com.bohush.ft.reader;
+import com.bohush.ft.exception.IntArrayException;
+
 import java.io.IOException;
 import java.util.List;
 
 public interface IntArrayReader {
-  List<String> readLines(String filePath) throws IOException;
+  List<String> readLines(String filePath) throws IntArrayException;
 }
--- src/main/java/com/bohush/ft/reader/impl/IntArrayReaderImpl.java
+++ src/main/java/com/bohush/ft/reader/impl/IntArrayReaderImpl.java
@@ -1,4 +1,5 @@
 package com.bohush.ft.reader.impl;
+import com.bohush.ft.exception.IntArrayException;
 import com.bohush.ft.reader.IntArrayReader;
 
 import java.io.IOException;
@@ -9,7 +10,11 @@ import java.util.List;
 public class IntArrayReaderImpl implements IntArrayReader {
 
   @Override
-  public List<String> readLines(String filePath) throws IOException {
-    return Files.readAllLines(Paths.get(filePath));
+  public List<String> readLines(String filePath) throws IntArrayException {
+    try {
+      return Files.readAllLines(Paths.get(filePath));
+    } catch (Exception e) {
+      throw new IntArrayException("Ошибка чтения файла: " + filePath, e);
+    }
   }
 }
